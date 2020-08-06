549a1e3f2e84762b9a5967d50c5b705f3db087eb
nick-devos <nickdevos123@yahoo.com>
2020-08-06 15:46:25 -0400

    part 2

--- src/main/java/com/github/perschola/MainApplication.java
+++ src/main/java/com/github/perschola/MainApplication.java
@@ -2,7 +2,13 @@ package com.github.perschola;
 
 public class MainApplication {
     public static void main(String[] args) {
-        Runnable myObject = new MyObject();
-        myObject.run();
+        Runnable task0 = new MyObject();
+        Thread thread0 = new Thread(task0);
+        Runnable task1 = new MyObject();
+        Thread thread1 = new Thread(task1);
+
+        thread0.start();
+        thread1.start();
+
     }
 }
--- src/main/java/com/github/perschola/MyObject.java
+++ src/main/java/com/github/perschola/MyObject.java
@@ -1,7 +1,17 @@
 package com.github.perschola;
 
+import java.util.stream.IntStream;
+
 public class MyObject implements Runnable {
     public void run() {
         // TODO
+        for(int i = 1; i < 5; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Sleep interrupted");
+            }
+            System.out.println(i);
+        }
     }
 }
