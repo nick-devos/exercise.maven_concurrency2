f1f04f3d73bce9bb3604d021a1fecdb6050ff992
nick-devos <nickdevos123@yahoo.com>
2020-08-07 23:23:29 -0400

    part 7

--- src/main/java/com/github/perschola/Waiter.java
+++ src/main/java/com/github/perschola/Waiter.java
@@ -1,8 +1,25 @@
 package com.github.perschola;
 
 public class Waiter implements Runnable {
+    private Message msg;
+    private String name;
+
+    public Waiter(Message message) {
+        this.msg = message;
+    }
+
     @Override
     public void run() {
-
+        name = Thread.currentThread().getName();
+        synchronized (msg) {
+            System.out.println(name + " waiting to get notified at time: " + System.currentTimeMillis());
+            try {
+                msg.wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Wait interrupted");
+            }
+            System.out.println(name + " waiter thread got notified at time: " + System.currentTimeMillis());
+            System.out.println(name + " processed: " + msg.getMessage());
+        }
     }
 }
